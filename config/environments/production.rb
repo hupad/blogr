6dccec18c0de1336e9cef688103bba262ce1e89f
Harish Upadhyayula <hupadhyayula@dealersocket.com>
2016-08-15 15:09:42 -0500

    Adding debugging to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.consider_all_requests_local = true
 
   config.action_mailer.default_url_options = { :host => 'aqueous-badlands-22607.herokuapp.com' }
 
